d3c893fcf9fa95a8a57c312635eced4b839e37be
paressuex11 <1138514846@qq.com>
2019-03-26 11:12:46 +0800

    s

---
+++ my_string.cpp
@@ -0,0 +1,5 @@
+#include"my_string.h"
+#include<cstring>
+my_string::my_string() {
+
+}
--- my_string.h
+++ my_string.h
@@ -4,8 +4,7 @@
 class my_string
 {
 private:
-	char* value;
-	int length;
+	char* value;            
 public:
 	int size ()const;
 	char& at(int index)const;
@@ -14,7 +13,8 @@ public:
 
 	my_string& operator =(const my_string& str);
 	my_string& operator+=(const my_string& str);
-	
+	my_string operator + (const my_string& str);
+
 	int compare(const my_string& str);
 	my_string substr(int start)const;
 	my_string substr(int start, int length)const;
@@ -22,12 +22,14 @@ public:
 	void replace(int start, int length, const my_string& str);
 	void erase(int start, int length);
 	void insert(int index, const my_string &str);
-	my_string operator + (const my_string& str);
+
 	my_string(const char * string);
 
 	~my_string();
 	my_string(const my_string& str);
 	friend std::ostream& operator << (std::ostream& output, const my_string& string);
+	
 };
 
 std::ostream& operator << (std::ostream& output, const my_string& string);
+std::istream& operator >> (std::istream& input, my_string& string);
